96156bd6eca41f9bb2e46da53058343bbcfb7ea5
levilu800b <levilu800b@gmail.com>
2022-11-15 08:58:01 +0000

    Exercise 3 completed

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,22 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+// - In `exercise-3.js`, you have been provided with a takeout order. Write a program that will print out the receipt for this order.
+// - Log each individual item to the console.
+// - Log the total cost of the order to the console.
+
+function receipt(order) {
+  // Your code here
+  let total = 0;
+  console.log("QTY        ITEM              TOTAL");
+  order.forEach((order) => {
+    const { itemName, quantity, unitPrice } = order;
+    let itemTotal = quantity * unitPrice;
+    total += itemTotal;
+    const space = " ".repeat(19 - itemName.length);
+    console.log(`${quantity}         ${itemName}${space}${itemTotal}`);
+  });
+}
+
+receipt(order);
