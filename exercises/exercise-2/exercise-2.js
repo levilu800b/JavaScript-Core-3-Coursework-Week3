4ac6de2b468d161beced03de61e5e9a68e1881e0
levilu800b <levilu800b@gmail.com>
2022-11-15 08:47:30 +0000

    Exercise 2 completed

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -70,3 +70,33 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+
+// In `exercise-2.js` write a program that will take the `hogwarts` array as input and display the names of the people who belong to the Gryffindor house.
+// - Use array destructuring to extract the values you need out of the array.
+
+function gryffindorNames(hogwarts) {
+  // Your code here
+  hogwarts.forEach((hogwarts) => {
+    const { firstName, lastName, house } = hogwarts;
+    if (house === "Gryffindor") {
+      console.log(`${firstName} ${lastName}`);
+    }
+  });
+}
+
+gryffindorNames(hogwarts);
+
+// - In `exercise-2.js` write a program that will take the `hogwarts` array as input and display the names of teachers who have pets.
+// - Use array destructuring to extract the values you need out of the array.
+
+function teachersWithPets(hogwarts) {
+  // Your code here
+  hogwarts.forEach((hogwarts) => {
+    const { firstName, lastName, pet, occupation } = hogwarts;
+    if (pet !== null && occupation === "Teacher") {
+      console.log(`${firstName} ${lastName}`);
+    }
+  });
+}
+
+teachersWithPets(hogwarts);
